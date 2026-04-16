3dc6d4e3b4a79c0579cc2d2c3c87c1f4d52a7171
«Artyom» <lzspector@mail.ru>
2026-04-16 23:11:18 +0500

    add test in processing.py

--- src/processing.py
+++ src/processing.py
@@ -35,3 +35,24 @@ def sort_by_date(data_list, reverse=True):
         key=lambda x: parse_date(x['date']),
         reverse=reverse
     )
+
+
+# Тестовые данные
+test_data = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+# Проверка filter_by_state
+print("Фильтрация по 'EXECUTED':")
+print(filter_by_state(test_data))
+print("\nФильтрация по 'CANCELED':")
+print(filter_by_state(test_data, 'CANCELED'))
+
+# Проверка sort_by_date
+print("\nСортировка по убыванию (по умолчанию):")
+print(sort_by_date(test_data))
+print("\nСортировка по возрастанию:")
+print(sort_by_date(test_data, reverse=False))
