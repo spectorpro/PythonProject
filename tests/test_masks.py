b916afb7dfb89aa6891148bfe405f15a39e178b9
«Artyom» <lzspector@mail.ru>
2026-04-23 23:11:36 +0500

    add test for get_mask_account

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -17,4 +17,20 @@ def card_with_dashes():
     return "1234 56** **** 3456"
 
 def test_card_with_dashes(card_with_dashes):
-    assert get_mask_card_number("1234567890123456") == card_with_dashes
+    assert get_mask_card_number("1234-567890-123456") == card_with_dashes
+
+def test_standard_20_digit_bank_account():
+    """Тест с обычным 20‑значным номером счета."""
+    result = get_mask_account("73654108430135874305")
+    assert result == "**4305"
+
+def test_card_with_spaces_bank_account():
+    """Тест с номером счета, содержащим нециофровые символы."""
+    result = get_mask_account("73f65410843f0135874305")
+    assert result == "**4305"
+
+def test_expected_length_bank_account():
+    """Тест с номером счета, содержащим меньшую длину."""
+    result = get_mask_account("7108430135874305")
+    assert result == "**4305"
+
